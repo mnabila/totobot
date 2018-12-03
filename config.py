058be04ed4c1b827bf420c1bc3ce25e6f8816440
nabil48 <nblid48@gmail.com>
2018-12-03 07:03:42 +0700

    first commit

---
+++ config.py
@@ -0,0 +1,7 @@
+config = {
+    "brand": "Xiaomi",
+    "meid": "3ec1cd703d1f86e8",
+    "os": "android",
+    "ticket": "sHp2r312otmCpnGns3mCqJhovNiBioTPsLqKobWlf868jYpqinavzI7cm2W0eIVjhbDSzICKgM27qomwv5N-lK2jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ",
+    "sign": "11cbaa088fa9b1d6d4be2150d52796b51d643d9fcebf040a8bd495f88675fe02"
+}
---
+++ run.py
@@ -0,0 +1,5 @@
+from bot import do_action
+import sys
+
+if __name__ == "__main__":
+    do_action(sys.argv[1:])
